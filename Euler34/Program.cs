0d18d392e4ae5d8a9f9da12562d17b1d84539d66
Jeremy Gibbons <jeremy.gibbons@laposte.net>
2015-10-03 06:05:33 +0200

    Initial Valid Solution

---
+++ Euler34/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Euler34
+{
+    class Program
+    {
+
+        static int[] factorials = new int[] {1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880 };
+
+        static void Main(string[] args)
+        {
+            List<int> results = new List<int>();
+            int n = 10;
+            while(n < 10000000)
+            {
+                int p = n;
+                int sum = 0;
+                while(p > 0)
+                {
+                    sum += factorials[p % 10];
+                    p = p / 10;
+                }
+                if(sum == n)
+                {
+                    Console.WriteLine(n);
+                    results.Add(n);
+                }
+                n++;
+            }
+            Console.WriteLine(results.Sum());
+            Console.ReadLine();
+        }
+    }
+}
